e1673ef61d763eb7c2a6671a9fad5f961147ab31
Tetrabiter <noa.eskenkot@gmail.com>
2025-09-30 23:35:16 +0500

    add a chart example , some changes in styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Sidebar } from "./components/Sidebar";
+import SimpleGanttChart from "../src/components/Gantt";
+import { ChartPieLegend } from "../src/components/PieChart";
 
 function App() {
   return (
-      <div className="flex">
-        <Sidebar />
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="w-full flex flex-col">
+        <SimpleGanttChart />
+        <ChartPieLegend />
       </div>
+    </div>
   );
 }
 
