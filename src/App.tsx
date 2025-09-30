6f4190f8c278cfbd15e3b0e298aaf2cbf101d30a
Tetrabiter <noa.eskenkot@gmail.com>
2025-09-30 16:57:33 +0500

    initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
+
+function App() {
+  return (
+      <div>
+        <h1 className="text-lime-300 text-2xl font-bold">Hello!</h1>
+      </div>
+  );
+}
+
+export default App;
