c77b3124e20d2f3726eee04f01fe7bfc56d03f97
elogrus <elogrus@gmail.com>
2025-10-01 20:36:02 +0500

    asddsaadsas

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,49 @@
-import { Outlet, useParams } from "react-router";
-import { AppSidebar } from "./components/app-sidebar";
-import { Breadcrumb } from "./components/ui/breadcrumb";
-import { Separator } from "./components/ui/separator";
-import {
-    SidebarInset,
-    SidebarProvider,
-    SidebarTrigger,
-} from "./components/ui/sidebar";
 import { useEffect } from "react";
-import { useStore } from "./stores/useStore";
+import { Outlet, useParams } from "react-router";
 import { AppHeader } from "./components/app-header";
+import { AppSidebar } from "./components/app-sidebar";
+import { SidebarInset, SidebarProvider } from "./components/ui/sidebar";
+import { http } from "./lib/http";
+import { useStore } from "./stores/useStore";
 
 function App() {
     const { logId } = useParams();
-    // const setSocket = useStore((state) => state.setSocket);
-    // useEffect(() => {
-    //     const socket = new WebSocket(
-    //         import.meta.env.VITE_BACKEND_WS
-    //     );
-    //     setSocket(socket);
-    //     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    //     socket.onopen = function (e) {
-    //         console.log("[open] Соединение установлено");
-    //         console.log("Отправляем данные на сервер");
-    //         socket.send("Меня зовут Саня");
-    //     };
+    const setSocket = useStore((state) => state.setSocket);
+    useEffect(() => {
+        http.get("set-user-id").then((res) => {
+            console.log("[set-user-id] куки-айди поставился");
+            const socket = new WebSocket(import.meta.env.VITE_BACKEND_WS);
+            setSocket(socket);
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            socket.onopen = function (e) {
+                console.log("[open] Соединение установлено");
+                console.log("Отправляем данные на сервер");
+                socket.send("Меня зовут Саня");
+            };
 
-    //     socket.onmessage = function (event) {
-    //         console.log(`[message] Данные получены с сервера: ${event.data}`);
-    //     };
+            socket.onmessage = function (event) {
+                console.log(
+                    `[message] Данные получены с сервера: ${event.data}`
+                );
+            };
 
-    //     socket.onclose = function (event) {
-    //         if (event.wasClean) {
-    //             console.log(
-    //                 `[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`
-    //             );
-    //         } else {
-    //             // например, сервер убил процесс или сеть недоступна
-    //             // обычно в этом случае event.code 1006
-    //             console.log("[close] Соединение прервано");
-    //         }
-    //     };
+            socket.onclose = function (event) {
+                if (event.wasClean) {
+                    console.log(
+                        `[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`
+                    );
+                } else {
+                    // например, сервер убил процесс или сеть недоступна
+                    // обычно в этом случае event.code 1006
+                    console.log("[close] Соединение прервано");
+                }
+            };
 
-    //     socket.onerror = function (error) {
-    //         console.log(`[error]`, error);
-    //     };
-    // }, []);
+            socket.onerror = function (error) {
+                console.log(`[error]`, error);
+            };
+        });
+    }, []);
 
     return (
         <div className="flex h-screen">
---
+++ src/lib/http.ts
@@ -0,0 +1,23 @@
+import ky from "ky";
+
+export interface ErrorResponse {
+    error: string;
+}
+
+export type Response<T> = T | ErrorResponse;
+
+export const http = ky.create({
+    prefixUrl: import.meta.env.VITE_BACKEND_HTTP,
+    timeout: 10000,
+    retry: 3,
+    credentials: "include",
+    hooks: {
+        beforeError: [
+            async (error) => {
+                const response = await error.response.json<ErrorResponse>();
+                if (response.error) error.message = response.error;
+                return error;
+            },
+        ],
+    },
+});
